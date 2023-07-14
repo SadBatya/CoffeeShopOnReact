c66692dceeb68282305c5ce097bd80f2587b2903
sadbatya <sadbatya@mail.ru>
2023-07-14 16:36:30 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import './App.css';
-import map from './img/Map.jpg';
-import headerImg from './img/Header_Image.png';
-import knowMore1 from './img/Know_more_Image_1.jpg';
-import knowMore2 from './img/Know_more_Image_2.jpg';
-import profileImg from './img/profileImg.png';
-import Button from './components/Button/Button';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Card from './components/Card/Card';
-import FoodCard from './components/FoodCard/FoodCard';
-import BlogCard from './components/BlogCard/BlogCard';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import AboutUs from './components/AboutUs/AboutUs';
